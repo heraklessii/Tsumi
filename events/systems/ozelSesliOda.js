431156d2513dcbb11696a205c33dcd225649532a
heraklessii <ilker34uckun@gmail.com>
2025-04-23 00:18:23 +0300

    LICENSE

--- events/systems/ozelSesliOda.js
+++ events/systems/ozelSesliOda.js
@@ -1,3 +1,18 @@
+// Tsumi Bot - Discord Bot Project
+// Copyright (C) 2025  Tsumi Bot Contributors
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const { ChannelType, ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder, Events, PermissionsBitField } = require('discord.js');
 const VoiceRoom = require('../../models/VoiceRoom');
 const VoiceSettings = require('../../models/VoiceSettings');
