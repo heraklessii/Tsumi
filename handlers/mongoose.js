d5880d24f875ceebe77d18302b88ad8805235f05
heraklessii <ilker34uckun@gmail.com>
2025-04-23 11:52:50 +0300

    ENV dosyasına MONGO eklendi.

--- handlers/mongoose.js
+++ handlers/mongoose.js
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+require('dotenv').config();
 const mongoose = require('mongoose');
-
 module.exports = (client) => {
 
     mongoose.connect(process.env.MONGO || "mongodb://localhost:27017/")
